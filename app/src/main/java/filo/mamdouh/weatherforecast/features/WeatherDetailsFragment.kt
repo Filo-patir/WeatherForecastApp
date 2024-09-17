6dfa9e0e0c3007a62f02ce809c009767c1a796c9
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-09-17 19:42:02 +0300

    fixed callback bug

--- app/src/main/java/filo/mamdouh/weatherforecast/features/WeatherDetailsFragment.kt
+++ app/src/main/java/filo/mamdouh/weatherforecast/features/WeatherDetailsFragment.kt
@@ -2,32 +2,33 @@ package filo.mamdouh.weatherforecast.features
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import filo.mamdouh.weatherforecast.R
 import filo.mamdouh.weatherforecast.databinding.FragmentWeatherDetailsBinding
 
 
 class WeatherDetailsFragment : Fragment() {
     lateinit var binding: FragmentWeatherDetailsBinding
-    val callback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            val bottomSheet = BottomSheetBehavior.from(binding.bottomSheet.standardBottomSheet)
-            if (BottomSheetBehavior.STATE_EXPANDED == bottomSheet.state)
-                bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
-            else {
-                this.remove()
-                requireActivity().onBackPressedDispatcher.onBackPressed()
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Filo", "onResume: ")
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val bottomSheet = BottomSheetBehavior.from(binding.bottomSheet.standardBottomSheet)
+                if (BottomSheetBehavior.STATE_EXPANDED == bottomSheet.state)
+                    bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+                else {
+                    this.remove()
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
             }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
@@ -38,12 +39,5 @@ class WeatherDetailsFragment : Fragment() {
         binding = FragmentWeatherDetailsBinding.inflate(inflater)
         return binding.root
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        callback.remove()
-    }
 }
