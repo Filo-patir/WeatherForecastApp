d5a02357be0089f1f015a49ae9768904d7f214f2
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-09-26 02:39:24 +0300

    made the alarm work even if the application is closed

--- app/src/main/java/filo/mamdouh/weatherforecast/features/alarm/AlarmReceiver.kt
+++ app/src/main/java/filo/mamdouh/weatherforecast/features/alarm/AlarmReceiver.kt
@@ -1,24 +1,18 @@
 package filo.mamdouh.weatherforecast.features.alarm
 
-import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import filo.mamdouh.weatherforecast.R
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val message = intent?.getStringExtra("message") ?: return
         Log.d("Filo", "onReceive: $message")
-        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notification = NotificationCompat.Builder(context, "alarm_channel").setContentText(message)
-            .setContentTitle("Alarm")
-            .setSmallIcon(R.mipmap.app_icon)
-            .setDefaults(NotificationCompat.DEFAULT_ALL)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .build()
-        notificationManager.notify(1, notification)
+        val workRequest = OneTimeWorkRequestBuilder<AlarmWorker>().setInputData(workDataOf("message" to message)).build()
+        WorkManager.getInstance(context!!).enqueue(workRequest)
     }
 }
